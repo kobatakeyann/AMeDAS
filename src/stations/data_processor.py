11707012ed85539844f4675b420c5f64008bde81
kobatakeyann <suu_hppa@outlook.jp>
2025-01-23 15:46:14 +0900

    Remove unnecessary instance variables

--- src/stations/data_processor.py
+++ src/stations/data_processor.py
@@ -6,13 +6,17 @@ from stations.parser import AmedasStationsParser
 
 class AmedasStationsDataProcessor:
     def __init__(self) -> None:
-        self._fetcher = AmedasStationsFetcher()
+        self._stations_html = AmedasStationsFetcher.fetch_station_page_html()
 
-    def save_stations_info(self, output_path) -> None:
-        staions_html = self._fetcher.fetch_station_page_html()
-        self._parser = AmedasStationsParser(staions_html)
-        area_info = self._parser.extract_areas()
-        stations_info = self._parser.get_stations_info(area_info)
-        self._arranger = AmedasStationsArranger(stations_info)
-        self._arranger.clean_stations_info()
-        self._arranger.save_stations_info_to_csv(output_path)
+    def save_stations_info(self, output_path: str) -> None:
+        """Fetch, process, and save AMeDAS stations information to a csv file.
+
+        Args:
+            output_path (str): Path to save the information as a csv file.
+        """
+        parser = AmedasStationsParser(self._stations_html)
+        area_info = parser.extract_areas()
+        stations_info = parser.get_stations_info(area_info)
+        arranger = AmedasStationsArranger(stations_info)
+        arranger.clean_stations_info()
+        arranger.save_stations_info_to_csv(output_path)
