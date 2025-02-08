86edc5a83070c03dfa1452d77e4d6e30c2415f87
kobatakeyann <suu_hppa@outlook.jp>
2025-02-09 00:48:10 +0900

    Update

--- src/util/date_formatter.py
+++ src/util/date_formatter.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime
 
 
 class PaddedDate:
@@ -16,3 +16,28 @@ class PaddedDate:
     @property
     def day(self) -> str:
         return f"{self._date.day:02d}"
+
+
+class PaddedDatetime:
+    def __init__(self, datetime: datetime) -> None:
+        self._datetime = datetime
+
+    @property
+    def year(self) -> str:
+        return f"{self._datetime.year:04d}"
+
+    @property
+    def month(self) -> str:
+        return f"{self._datetime.month:02d}"
+
+    @property
+    def day(self) -> str:
+        return f"{self._datetime.day:02d}"
+
+    @property
+    def hour(self) -> str:
+        return f"{self._datetime.hour:02d}"
+
+    @property
+    def minute(self) -> str:
+        return f"{self._datetime.minute:02d}"
